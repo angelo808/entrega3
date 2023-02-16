4bbda466f8e949abc0b43729222cf6413be628a9
GarroSebastian <garrosebastian07@gmail.com>
2023-02-16 15:01:34 -0500

    Añadir categoria a restaurantes

--- endpoints/models.py
+++ endpoints/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    CATEGORIA_ESTADOS = (
+        ("A", "Activo"),
+        ("I", "Inactivo")
+    )
+    nombre = models.CharField(max_length=50)
+    estado = models.CharField(max_length=1, choices=CATEGORIA_ESTADOS)
+
+    def __str__(self):
+        return self.nombre
+
+
 class Carrito(models.Model):
     CARRITO_ESTADOS = (
         ("A", "Activo"),
@@ -18,18 +31,9 @@ class Carrito(models.Model):
 class Restaurante(models.Model):
     nombre = models.CharField(max_length=100)
     url = models.URLField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.nombre
 
 
-class Categoria(models.Model):
-    CATEGORIA_ESTADOS = (
-        ("A", "Activo"),
-        ("I", "Inactivo")
-    )
-    nombre = models.CharField(max_length=50)
-    estado = models.CharField(max_length=1, choices=CATEGORIA_ESTADOS)
-
-    def __str__(self):
-        return self.nombre
--- entrega3/urls.py
+++ entrega3/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from endpoints import views
-from . import views
+
 
 
 urlpatterns = [
