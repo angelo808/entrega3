92adfda8d08ab05ed079c4a2959a8febffff6f19
GarroSebastian <garrosebastian07@gmail.com>
2023-02-16 13:34:31 -0500

    Edicion del merge

--- entrega3/urls.py
+++ entrega3/urls.py
@@ -21,7 +21,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("endpoints/", include("endpoints.urls"))
+    path("endpoints/", include("endpoints.urls")),
     path("codigopedido", views.codigopedido),
     path("login", views.login)
 ]
